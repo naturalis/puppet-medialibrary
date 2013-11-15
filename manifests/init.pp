9338548f76454ac31dafc9f1b30ff3f2164f277c
Atze de Vries <atze.devries@naturalis.nl>
2013-11-15 10:31:46 +0100

    Initialization of Medialibrary Puppet Module * Get code * create streets * shares with samba (although not fully functional yet)

---
+++ manifests/init.pp
@@ -0,0 +1,150 @@
+# == Class: medialibrary
+#
+# Full description of class medialibrary here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { medialibrary:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2013 Your name here, unless otherwise noted.
+#
+class medialibrary (
+  
+  $base_data_dir                      = '/data',
+  $base_www_dir                       = '/data/www',
+  $base_masters_dir                   = '/data/masters',
+
+  $db_host                            ,
+  $db_user                            ,
+  $db_password                        ,
+  $db_dbname                          ,
+
+  $numBackupGroups                    = 1,
+
+
+  $offload_immediate                  = 'true',
+  $offload_method                     = 'CUSTOM',
+  $offload_tar_maxSize                = 1000,
+  $offload_tar_maxFiles               = 0,
+  $offload_ftp_host                   ,
+  $offload_ftp_user                   ,
+  $offload_ftp_password               ,
+  $offload_ftp_passive                = 'false',
+  $offload_ftp_reconnectPerFile       = 'false',
+  $offload_ftp_maxConnectionAttempts  = 3,
+  $offload_ftp_maxUploadAttempts      = 3,
+
+  $resizeWhen_fileType                = 'tiff,jpg,tif,jpeg,gif,png',
+  $resizeWhen_imageSize               =  3000,
+
+  $imagemagick_convertCommand         = 'convert \"%s\" \"%s\"',
+  $imagemagick_resizeCommand          = 'convert \"%s\" -resize 3000x3000^> -quality 80 \"%s\"',
+  $imagemagick_maxErrors              = 0,
+
+  $imagemagick_command                = 'convert',
+  $imagemagick_large_size             = 1920,
+  $imagemagick_large_quality          = 100,
+  $imagemagick_medium_size            = 500,
+  $imagemagick_medium_quality         = 100,
+  $imagemagick_small_size             = 100,
+  $imagemagick_small_quality          = 100,
+
+  $cleaner_minDaysOld                 = 4,
+  $cleaner_sweep                      = 'false',
+  $cleaner_unixRemove                 = 'true',
+
+  $svn_loc                            = 'svn://dev2.etibioinformatics.nl/NBCMediaLib/MediaPublisher/trunk',
+  $svn_revision                       = 'latest',
+  ) { 
+
+  if $operatingsystem != 'ubuntu' {
+    fail('Only ubuntu is supported')
+  }
+  file { "/etc/medialibrary":
+    ensure => directory,
+  }
+  
+  file { [ $base_data_dir,
+           $base_masters_dir,
+           $base_www_dir ]:    
+    ensure => directory 
+  }
+  
+  host { "${hostname}.nnm.local":
+    ip => '127.0.0.1',
+    host_aliases => [ $hostname ],
+  }
+
+  package { ['subversion','imagemagick','ncftp','php5','php5-mysql']: ensure => installed, }
+  
+  if $svn_revision == 'latest' {
+    vcsrepo { '/opt/medialibrary':
+      ensure   => latest,
+      provider => svn,
+      source   => $svn_loc,
+      require  => [ Package['subversion'],Host["${hostname}.nnm.local"] ],
+    }
+  }else{
+      vcsrepo { '/opt/medialibrary':
+      ensure   => present,
+      provider => svn,
+      revision => $svn_revision,
+      source   => $svn_loc,
+      require  => [ Package['subversion'],Host["${hostname}.nnm.local"] ],
+    }
+  }
+
+  file {"/etc/medialibrary/ftp.cfg":
+    ensure  => present,
+    require => File["/etc/medialibrary"],
+    content => template("medialibrary/ftp.cfg.erb")
+  }
+
+
+  create_resources('medialibrary::street', hiera('medialibrary::street', []))
+
+  class {'samba::server':
+    workgroup => 'NNM',
+    server_string => "ml-test",
+    interfaces => "eth0 lo",
+    security => 'ads',
+    require => Host["${hostname}.nnm.local"],
+  }
+  
+  class { 'samba::server::ads':
+     winbind_acct    => '',
+     winbind_pass    => '',
+     realm           => '',
+     nsswitch        => true,
+     target_ou       => "Computers",
+     require         => Class['samba::server']
+  }
+
+}
---
+++ manifests/street.pp
@@ -0,0 +1,135 @@
+define medialibrary::street(
+  $street                 = $title,
+  $producer               ,               
+  $owner                  = 'undefined',
+  $fileTypes              = 'jpg,jpeg,tiff,tif,png',
+  $log_stdout             = 'true',
+  $log_level              = 'INFO',
+  $mail_to                ,
+  $mail_onsuccess         = 'true' 
+  
+) {
+  
+  # variables from main (and more general)
+  
+
+  $base_data_dir                      = $medialibrary::base_data_dir
+  $masterDirectory                    = $medialibrary::base_www_dir
+  $wwwDirectory                       = $medialibrary::base_masters_dir
+
+  $db_host                            = $medialibrary::db_host
+  $db_user                            = $medialibrary::db_user
+  $db_password                        = $medialibrary::db_password
+  $db_dbname                          = $medialibrary::db_dbname
+
+  $numBackupGroups                    = $medialibrary::numBackupGroups
+  $offload_immediate                  = $medialibrary::offload_immediate 
+  $offload_method                     = $medialibrary::offload_method 
+  $offload_tar_maxSize                = $medialibrary::offload_tar_maxSize
+  $offload_tar_maxFiles               = $medialibrary::offload_tar_maxFiles 
+  $offload_ftp_host                   = $medialibrary::offload_ftp_host
+  $offload_ftp_user                   = $medialibrary::offload_ftp_user
+  $offload_ftp_password               = $medialibrary::offload_ftp_password 
+  $offload_ftp_passive                = $medialibrary::offload_ftp_passive
+  $offload_ftp_reconnectPerFile       = $medialibrary::offload_ftp_reconnectPerFile
+  $offload_ftp_maxConnectionAttempts  = $medialibrary::offload_ftp_maxConnectionAttempts
+  $offload_ftp_maxUploadAttempts      = $medialibrary::offload_ftp_maxUploadAttempts
+
+  $resizeWhen_fileType                = $medialibrary::resizeWhen_fileType
+  $resizeWhen_imageSize               = $medialibrary::resizeWhen_imageSize
+
+  $imagemagick_convertCommand         = $medialibrary::imagemagick_convertCommand
+  $imagemagick_resizeCommand          = $medialibrary::imagemagick_resizeCommand 
+  $imagemagick_maxErrors              = $medialibrary::imagemagick_maxErrors
+
+  $imagemagick_command                = $medialibrary::imagemagick_command
+  $imagemagick_large_size             = $medialibrary::imagemagick_large_size
+  $imagemagick_large_quality          = $medialibrary::imagemagick_large_quality
+  $imagemagick_medium_size            = $medialibrary::imagemagick_medium_size
+  $imagemagick_medium_quality         = $medialibrary::imagemagick_medium_quality
+  $imagemagick_small_size             = $medialibrary::imagemagick_small_size
+  $imagemagick_small_quality          = $medialibrary::imagemagick_small_quality
+
+  $cleaner_minDaysOld                 = $medialibrary::cleaner_minDaysOld
+  $cleaner_sweep                      = $medialibrary::cleaner_sweep
+  $cleaner_unixRemove                 = $medialibrary::cleaner_unixRemove
+
+
+
+  #logical derivitives
+
+  $offload_ftp_initDir                = $producer
+  $debug_maxFiles                     = 0
+
+  $containerDirectory                 = "${medialibrary::base_data_dir}/${street}.street"
+  $publicDirectory                    = "${medialibrary::base_data_dir}/${street}.street/${street}"
+  $harvestDirectory                   = "${publicDirectory}/harvest"
+  $duplicatesDirecotry                = "${publicDirectory}/duplicates"
+  $resubmitDirectory                  = "${publicDirectory}/resubmit"
+  $productionDirectory                = "${publicDirectory}/production"
+  $stagingDirectory                   = "${containerDirectory}/staging"
+  $logDirectory                       = "${containerDirectory}/log"
+  $deadImagesDirectory                = "${publicDirectory}/errors"
+
+  
+  file { [ $containerDirectory,
+           $publicDirectory,
+           $productionDirectory,
+           $harvestDirectory,     
+           $duplicatesDirecotry,  
+           $resubmitDirectory,    
+           $stagingDirectory,     
+           $logDirectory,         
+           $deadImagesDirectory ]:
+    ensure => directory,
+    require => File[$medialibrary::base_data_dir],  
+  }
+
+
+  if $offload_method == 'CUSTOM' {
+    $offload_command = "tar chPf - \"%local_dir%\" | ncftpput -c -f /etc/medialib/ftp.cfg %remote_dir%/%name%"
+  }
+
+  if !defined(File['/etc/medialibrary']) {
+  	file { "/etc/medialibrary":
+      ensure => directory,
+    }
+  }
+  
+  $nb = $medialibrary::numBackupGroups -1
+
+  if !defined(Cron['offload']) {
+    $offload_job = "for i in {0..${nb}}; do /usr/bin/php /opt/medialibrary/offload.php /etc/medialibrary/config-${street}.ini $i & ; done"
+    cron { 'offload':
+      ensure  => present,
+      command => $offload_job,
+      user    => root,
+      hour    => ['18-8'],
+      minute  => '*/10',
+    }
+  }
+     
+  #if !defined(File[$medialibrary::base_data_dir]) {
+  #   file { [ $medialibrary::base_data_dir,
+  #            $medialibrary::base_masters_dir,
+  #            $medialibrary::base_www_dir ]:    
+  #    ensure => directory 
+  #  }
+  #}
+
+  file {"/etc/medialibrary/config-${street}.ini":
+    ensure  => present,
+    require => File["/etc/medialibrary"],
+    content => template("medialibrary/config.ini.erb")
+  }
+
+  samba::server::share { $street:
+    comment           => "Share for ${street}",
+    path              => $publicDirectory,
+    browsable         => true,
+    writable          => true,
+    write_list       => '@"NNM\Domain Users"',
+    create_mask       => 0770,
+    directory_mask    => 0770,
+  }
+}
