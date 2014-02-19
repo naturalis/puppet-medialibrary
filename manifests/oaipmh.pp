cddd2a0ff4d71e53cc8a763df74e3f588d571bf5
Atze de Vries <atze.devries@naturalis.nl>
2014-02-19 10:19:39 +0100

    trying augeas on vhost

--- manifests/oaipmh.pp
+++ manifests/oaipmh.pp
@@ -41,7 +41,7 @@ class medialibrary::oaipmh (
       changes => [
         "set \"/VirtualHost/*/[self::directive=\'ProxyHTMLURLMap\']/arg\" \"/oai-pmh /medialib/oai-pmh\" "
       ],
-      require => Apache::vhost["$external_web_address"],
+      require => File["1-${external_web_address}.conf"],
     }
   }
 
