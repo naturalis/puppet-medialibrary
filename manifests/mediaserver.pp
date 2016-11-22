569608b9af740e98874a143aff17cb4d9b4da7c2
Atze de Vries <atze.devries@naturalis.nl>
2016-11-22 11:12:09 +0100

    updated mediaserver from the nineties

--- manifests/mediaserver.pp
+++ manifests/mediaserver.pp
@@ -1,77 +1,41 @@
+#
+#
+#
 class medialibrary::mediaserver (
-  $cluster_name                       ,
   $db_host                            ,
   $db_mediaserver_user                ,
   $db_mediaserver_password            ,
   $db_dbname                          ,
-
-  $media_harvester_ip                 ,
-
+  $dataserver_ip                      ,
   $media_server_url                   ,
-
+  $deploykey                          ,
   $base_data_dir                      = '/data',
   $base_www_dir                       = '/data/www',
   $base_masters_dir                   = '/data/masters',
-
-  $svn_loc                            = 'svn://dev2.etibioinformatics.nl/NBCMediaLib/MediaServer/trunk',
-  $svn_revision                       = 'latest',
-
-  $log_directory                      = '/var/www/mediaserver/log',
+  $log_directory                      = '/var/log/mediaserver/',
   ) {
 
-  
-  include concat::setup
-  include nfs::server
-  
-  class { 'selinux':
-    mode => 'disabled',
+
+  package { 'git':
+    ensure => installed,
   }
-
-  nfs::server::export{ $base_www_dir :
-      clients => "${media_harvester_ip}(rw,sync,no_root_squash)",
-      nfstag  => "${cluster_name}_mediaserver_www_directory",
-      require => File[$base_www_dir],
-  }
-
-  case $::operatingsystem {
-    centos, redhat: {
-      package {['subversion',
-                'ImageMagick',
-                'php-mysql',
-                'php-gd',
-                'sendmail']:
-        ensure => installed,
-      }
-    }
-    debian, ubuntu: {
-      package {['subversion',
-                'imagemagick',
-                'php5-mysql',
-                'sendmail']:
-        ensure => installed,
-      }
-    }
-
-    default: {
-      fail('Unrecognized operating system')
-    }
-  }
-
-# Include apache modules with php
+  # Include apache modules with php
   class { 'apache':
-    default_mods  => true,
-    mpm_module    => 'prefork',
+    default_mods => true,
+    mpm_module   => 'prefork',
   }
 
-  class{ 'apache::mod::php': 
-  }
+  class{ 'apache::mod::php': }
 
   apache::vhost { $media_server_url:
-      port            => '80',
-      docroot         => '/var/www/mediaserver',
-      servername      => $media_server_url,
-      directories     => [{ path => '/var/www/mediaserver',allow_override => 'All' } ],
-      require         => Vcsrepo['/var/www/mediaserver'],
+      port        => '80',
+      docroot     => '/var/www/mediaserver',
+      servername  => $media_server_url,
+      directories => [{
+        path           => '/var/www/mediaserver',
+        allow_override => 'All' }
+        ],
+      require     => Vcsrepo['/var/www/mediaserver'],
   }
 
   file { [$base_data_dir,
@@ -82,27 +46,20 @@ class medialibrary::mediaserver (
   }
 
   host { $::hostname :
-    ip            => '127.0.0.1',
-    host_aliases  => [ $::hostname,$media_server_url ],
+    ip           => '127.0.0.1',
+    host_aliases => [ $::hostname,$media_server_url ],
   }
 
+  class {'::medialibrary::deploykey':
+    key => $deploykey,
+  }
 
-
-  if $svn_revision == 'latest' {
-    vcsrepo { '/var/www/mediaserver':
-      ensure   => latest,
-      provider => svn,
-      source   => $svn_loc,
-      require  => [Package['subversion'],Host[$::hostname],File['/var/www']],
-    }
-  }else{
-      vcsrepo { '/var/www/mediaserver':
-      ensure   => present,
-      provider => svn,
-      revision => $svn_revision,
-      source   => $svn_loc,
-      require  => [Package['subversion'],Host[$::hostname],File['/var/www']],
-    }
+  vcsrepo { '/var/www/mediaserver':
+    ensure   => present,
+    provider => 'git',
+    source   => 'git@github.com:naturalis/MediaServer.git',
+    user     => 'root',
+    require  => [Package['git'],Class['::medialibrary::deploykey']],
   }
 
   file {'/var/www/mediaserver/static.ini':
@@ -114,8 +71,9 @@ class medialibrary::mediaserver (
 
   file {$log_directory:
     ensure  => directory,
-    mode    => '0666',
+    mode    => '0660',
     require => Vcsrepo['/var/www/mediaserver'],
+    group   => 'www-data',
   }
 
 }
