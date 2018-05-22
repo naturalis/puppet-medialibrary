b942560ef4a40ced7aa87f1cc1281f61b6247954
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-05-22 09:03:16 +0000

    added ssl

--- manifests/mediaserver.pp
+++ manifests/mediaserver.pp
@@ -47,6 +47,22 @@ class medialibrary::mediaserver (
 
   }
 
+# SSL vhost at the same domain
+  apache::vhost { "${media_server_url} ssl":
+    port       => '443',
+    servername      => $media_server_url,
+    custom_fragment => "XSendFile on\nXSendFilePath ${base_masters_dir}",
+    docroot    => '/var/www/mediaserver',
+    directories     => [{
+        path           => '/var/www/mediaserver',
+        allow_override => 'All' }
+        ],
+    ssl        => true,
+    ssl_cert   => '/etc/ssl/certs/STAR_naturalis_nl.pem',
+    ssl_key    => '/etc/ssl/private/STAR_naturalis_nl.key',
+    require    => Apache::Vhost[$media_server_url],
+  }
+
   file { [$base_data_dir,
           $base_masters_dir,
           $base_www_dir,
